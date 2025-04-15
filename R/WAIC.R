430d0e9d5b455b84c8efc0ccb9bbab30275a4b87
Achaz von Hardenberg <achaz.hardenberg@unipv.it>
2025-04-15 23:07:53 +0200

    Create WAIC.R

---
+++ R/WAIC.R
@@ -0,0 +1,14 @@
+WAIC<-function(model,n.iter = 2000,n.burnin = 500,n.thin = 10){
+  samples <- jags.samples(model$model,
+                              c("WAIC","deviance"),
+                              type = "mean",
+                              n.iter = n.iter,
+                              n.burnin = n.burnin,
+                              n.thin = n.thin)
+
+  samples$p_waic <- samples$WAIC
+  samples$waic <- samples$deviance + samples$p_waic
+  tmp <- sapply(samples, sum)
+  waic <- round(c(waic = tmp[["waic"]], p_waic = tmp[["p_waic"]]),1)
+  return(waic)
+}
